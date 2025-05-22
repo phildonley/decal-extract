8fecfb4953682fc7f0ee351a7f88f11b0037ac6a
phildonley <phillipdonleyterex@gmail.com>
2025-05-22 07:01:03 -0700

    Update DecalExtract.py

--- DecalExtract.py
+++ DecalExtract.py
@@ -21,7 +21,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import WebDriverException
+from selenium.common.exceptions import WebDriverException, NoSuchElementException
 
 # ── Configuration ──────────────────────────────────────────────────────────────
 SITE_ID         = 733
